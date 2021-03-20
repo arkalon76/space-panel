b8fde2bfb62263a4835ecc922205f5a37ac7e7ac
Kenth Fagerlund <kenth@divit.com.hk>
2021-03-21 07:34:14 +0800

    Comment

--- arduino/btnmatrix.ino
+++ arduino/btnmatrix.ino
@@ -4,15 +4,24 @@
 const byte ROWS = 3; 
 const byte COLS = 3; 
 
+/*
+  Keyboard layout for KSP.
+  ------------------------
+  <space> [Trigger next stage]  => 7,16
+  <T>     [Toggle SAS]          => 8,14
+  <R>     [Toggle RCS]          => 8,15
+*/
 char hexaKeys[ROWS][COLS] = {
   {'1', '2', ' '},
   {'T', 'R', 'A'},
   {'7', '8', 'D'},
 };
 
+// Link to correct pins
 byte rowPins[ROWS] = {7, 8, 9};
 byte colPins[COLS] = {14,15,16}; 
 
+//Setup keypad
 Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
 
 void setup(){
